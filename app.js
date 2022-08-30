a3760621e9f4364640fee9f8b256866910cc74d7
Yarosurawa <93207497+Yarosurawa@users.noreply.github.com>
2022-08-30 20:37:38 +0300

    update

--- app.js
+++ app.js
@@ -2,14 +2,30 @@ const dn = document.querySelectorAll('.downer')
 const up = document.querySelectorAll('.upper')
 const svg = document.querySelector('svg')
 const img = document.querySelector('img')
-
+let open = false;
 
 svg.onclick = function () {
+    if (open) {
+    up[0].style.opacity = "1"
+    up[1].style.opacity = "1"
+    up[0].style.transform = "translate(0, 0)"
+    up[1].style.transform = "translate(0, 0)"
+    dn[0].style.transform = "translate(0, 0)"
+    dn[1].style.transform = "translate(0, 0)"
+    dn[2].style.transform = "translate(0, 0)"
+    img.style.translate = "0 -0"
+    img.style.transform = "scale(1)"
+    open = false;
+    } else{
     up[0].style.transform = "translate(0, -270px)"
     up[1].style.transform = "translate(0, -270px)"
+    up[0].style.opacity = "0"
+    up[1].style.opacity = "0"
     dn[0].style.transform = "translate(0, 20px)"
     dn[1].style.transform = "translate(0, 20px)"
     dn[2].style.transform = "translate(0, 20px)"
     img.style.translate = "0 -180px"
     img.style.transform = "scale(5.5)"
+    open = true;
+    }
 }
